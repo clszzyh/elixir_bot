f632425eff46b03fefc344cfd565d65bcf256ac9
clszzyh <clszzyh@gmail.com>
2020-12-31 14:08:24 +0800

    init
    Signed-off-by: clszzyh <clszzyh@gmail.com>

---
+++ lib/elixir_bot.ex
@@ -0,0 +1,18 @@
+defmodule ElixirBot do
+  @moduledoc """
+  Documentation for `ElixirBot`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirBot.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
