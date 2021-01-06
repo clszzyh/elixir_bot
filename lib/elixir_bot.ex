38a4886b848b5fd5906daf9a865384bfa3bd306c
clszzyh <clszzyh@gmail.com>
2021-01-06 12:02:27 +0800

    info
    Signed-off-by: clszzyh <clszzyh@gmail.com>

--- lib/elixir_bot.ex
+++ lib/elixir_bot.ex
@@ -7,45 +7,41 @@ defmodule ElixirBot do
   def version, do: @version
 
   require Logger
+  alias Action.Api
+  alias Action.Github
 
-  # @typep result :: :ok | :ignore
+  @typep result :: :ok | :ignore
 
   @spec main :: :ok
   def main do
-    Logger.configure(truncate: :infinity)
-    Logger.info(inspect(System.get_env(), pretty: true))
-
-    github = Action.Github.init()
-    Logger.info(inspect(github, pretty: true))
-    Logger.configure(truncate: 8192)
+    %Github{} = github = Github.init()
+    github |> handle() |> result(github)
     :ok
     # opt |> handle() |> result(opt)
   end
 
-  # @spec handle(t()) :: result()
-  # def handle(%__MODULE__{
-  #       event_name: "issue_comment",
-  #       event: %{action: action, comment: %{body: body}}
-  #     })
-  #     when action in ["created"] do
-  #   Logger.warn(action: action, body: body)
-  #   :ok
-  # end
+  @spec handle(Github.t()) :: result()
+  def handle(%{event_name: "issue_comment", event: %{action: action, comment: %{body: body}}})
+      when action in ["created"] do
+    Api.debug(inspect({action, body}))
+    :ok
+  end
 
-  # def handle(%__MODULE__{event_name: "issues", event: %{action: action, issue: %{body: body}}})
-  #     when action in ["opened", "edited"] do
-  #   Logger.warn(action: action, body: body)
-  #   :ok
-  # end
+  def handle(%{event_name: "issues", event: %{action: action, issue: %{body: body}}})
+      when action in ["opened", "edited"] do
+    Api.debug(inspect({action, body}))
+    :ok
+  end
 
-  # def handle(_), do: :ignore
-  # @spec result(result(), t()) :: :ok
-  # defp result(:ok, %__MODULE__{event_name: event_name}) do
-  #   Logger.info("[ok] #{event_name}")
-  #   :ok
-  # end
-  # defp result(:ignore, %__MODULE__{event_name: event_name}) do
-  #   Logger.info("[ignore] #{event_name}")
-  #   :ok
-  # end
+  def handle(_), do: :ignore
+  @spec result(result(), Github.t()) :: :ok
+  defp result(:ok, %{event_name: event_name}) do
+    Api.debug("[ok] #{event_name}")
+    :ok
+  end
+
+  defp result(:ignore, %{event_name: event_name}) do
+    Api.error("[ignore] #{event_name}")
+    :ok
+  end
 end
